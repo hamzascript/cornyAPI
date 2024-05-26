5831e584518be2812790abe1103da58cd1c2644c
Hamza Elashry <98109755+hamzascript@users.noreply.github.com>
2024-05-25 23:55:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,4 +11,21 @@ app.get("/", (req, res) => {
   res.send(list[randomIndex]);
 });
 
+app.get('/memes', async (req, res) => {
+  const memes = require("random-memes");
+
+  const fetchMeme = async () => {
+    const meme = await memes.reddit({ locale: "en" });
+    return meme
+  };
+
+  try {
+    const meme = await fetchMeme();
+    res.json(meme);
+  } catch (error ) {
+    console.error(error)
+    res.status(500).json({ error: 'failed to fetch meme'})
+  }
+})
+
 app.listen(8080, () => console.log("running joke api"));
